d014aabae11d9202ea5e5e73b9b56d82a589641e
LukazAllan <allanlucas130@gmail.com>
2026-09-15 20:39:43 -0300

    Aula 15/Set

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,20 @@
+package med.voll.api.infra.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+    public String gerarToken() {
+        /* Cenas do Próximo Capítulo
+        try {
+            Algorithm algorithm = Algorithm.RSA256(rsaPublicKey, rsaPrivateKey);
+            String token = JWT.create()
+                    .withIssuer("auth0")
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            // Invalid Signing configuration / Couldn't convert Claims.
+        }
+        */
+        return "";
+    }
+}
